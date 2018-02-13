ca10f1f90a58c41b817add1a30299e4e30d1e6d2
James McCormack <thejamesmccormack@gmail.com>
2018-02-13 14:28:30 -0800

    new configuration for standalone react server with webpack

---
+++ webpack.config.common.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+plugins: [
+    new CleanWebpackPlugin(['app']),
+    new HtmlWebpackPlugin({
+      title: 'Hiking Club',
+      filename: 'index.html',
+      template: './public/index.html',
+    }),
+  ],
+module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'es2015',
+              'react',
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              'es2015',
+              'react',
+            ],
+          },
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'app'),
+  },
+};
