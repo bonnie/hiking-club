c9921833a7af3abd321265b6a243302675986240
James McCormack <thejamesmccormack@gmail.com>
2018-02-15 13:51:46 -0800

    change journal component render to implicit return

--- src/Components/Journal/Journal.js
+++ src/Components/Journal/Journal.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Journal.scss';
 
 const Journal = function(props) {
+  console.log('review journals', props);
   return (
     <div>
-      <h2>{props.title}</h2>
-      <p>{props.entry}</p>
+      <h2>Title: {props.title} </h2>
+      <p>Entry: {props.entry} </p>
     </div>
   );
-}
+};
 
 export default Journal;
--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -2,9 +2,10 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Review from '../Review/Review';
 import Journal from '../Journal/Journal';
+
 import './Profile.scss';
 
-class Profile extends Component {
+export default class Profile extends Component {
 
   constructor(props) {
     super(props);
@@ -15,16 +16,13 @@ class Profile extends Component {
     };
   };
 
-  // loads reviews as soon as page loads.
-  componentDidMount() {
-    this.getReviews();
-    this.getJournals();
-  }
-
   // gets all of the reviews for a user.
   getReviews() {
     // dummy data until api is hooked up.
-    const fakeReviews = [{id: 1, user_id: 4, rating: 4, comment: "great!"}, {id: 2, user_id: 1, rating: 3, comment: "blah!"}];
+    const fakeReviews = [
+      {id: 1, user_id: 4, rating: 4, comment: "great!"},
+      {id: 2, user_id: 1, rating: 3, comment: "blah!"}
+    ];
     // axios.get(`https://localhost:3000/users/${this.state.userId}/reviews`)
     //   .then((reviews) => {
       this.setState({
@@ -34,32 +32,85 @@ class Profile extends Component {
       // .catch(console.error);
   };
 
-  // allows the user to edit a review. need to remember how to pass in the value of the review id from the click event.
-  editReview() {
-    axios.put(`https://localhost:3000/users/${this.state.userId}/:id`)
-      .then((editedReview) => {
-        // set the state of the specific review and then update the reviews array.
-      })
-      .catch(console.error)
-  }
-
   // gets all of the journals for a user.
   getJournals() {
-    // dummy data until api is hooked up. this is currently not working
-    const fakeJournals = [{id: 1, trail_id: 2, user_id: 4, title: "Worst Hike Ever!", entry: "I got stung by angry bees."}, {
-      id: 2, user_id: 2, title: "I love trees!", entry: "Trees are the best!"}];
-    console.log('fakeJournals', fakeJournals);
-    // axios.get(`https://localhost:3000/users/${this.state.userId}/journals`)
-    //   .then((journals) => {
-    this.setState({
-      journals: fakeJournals,
-    });
-    console.log('this.state.journals', this.state.journals);
+    // dummy data until api is hooked up.
+    const fakeJournals = [
+      {id: 1, trail_id: 1, user_id: 1, title: 'great trail!', entry: 'really nice.'}
+    ];
+    // axios.get(`https://localhost:3000/users/${this.state.userId}/reviews`)
+    //   .then((reviews) => {
+      this.setState({
+        journals: fakeJournals,
+      });
       // })
       // .catch(console.error);
   };
 
+  getAllUserContent() {
+    this.getReviews();
+    this.getJournals();
+  }
+
+  // loads reviews as soon as page loads.
+  componentDidMount() {
+    this.getAllUserContent();
+  }
+
+  // allows the user to edit a review. need to remember how to pass in the value of the review id from the click event.
+  // editReview() {
+  //   axios.put(`https://localhost:3000/users/${this.state.userId}/:id`)
+  //     .then((editedReview) => {
+  //       // set the state of the specific review and then update the reviews array.
+  //     })
+  //     .catch(console.error)
+  // }
+
   render() {
+
+    let reviewList = null;
+
+    if (this.state.reviews.length > 0) {
+      reviewList = (
+        <div>
+        {this.state.reviews.map((review, index) =>
+          <Review
+          key={review.id}
+          userId={review.user_id}
+          rating={review.rating}
+          comment={review.comment}
+          />
+        )}
+        </div>
+      );
+    } else {
+      reviewList = (
+        <div>No reviews here. Why not create one?</div>
+      );
+    }
+
+    let journalList = null;
+
+    if (this.state.journals.length > 0) {
+      journalList = (
+        <div>
+        {this.state.journals.map((journal, index) =>
+          <Journal
+            key={journal.id}
+            trailId={journal.trail_id}
+            user_id={journal.user_id}
+            title={journal.title}
+            entry={journal.entry}
+          />
+        )}
+      </div>
+    );
+    } else {
+      journalList = (
+        <div>No journals yet. Why not create one?</div>
+      )
+    }
+
     return (
       <div className="profile-container">
         <div className="user-profile">
@@ -69,35 +120,13 @@ class Profile extends Component {
         </div>
         <div className="user-content">
           <div className="review-container">
-            {this.state.reviews.map((review, index) =>
-              <Review
-                key={review.id}
-                id={review.id}
-                userId={review.user_id}
-                rating={review.rating}
-                comment={review.comment}
-              />
-            )}
+            {reviewList}
           </div>
           <div className="journal-container">
-            {this.state.journals.map((journal, index) => {
-              <Journal
-                key={journal.id}
-                id={journal.id}
-                trailId={journal.trail_id}
-                user_id={journal.user_id}
-                title={journal.title}
-                entry={journal.entry}
-              />
-            })}
+            {journalList}
           </div>
         </div>
       </div>
     );
   }
 };
-
-          
-
-
-
