a655a28afce55ef3eed511147dfb40b162869209
James McCormack <thejamesmccormack@gmail.com>
2018-03-01 13:33:30 -0800

    set up boilerplate files, folders and packages for redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './app'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import App from './app';
+import reducers from './reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>, document.getElementById('root'))
+
 
 if (module.hot) {
   module.hot.accept();
