afb7c4ee0e32f78e53575a63617b5e85d70c510b
LaToya Williams <latoyawilliams@LaToyas-MacBook-Pro.local>
2018-03-01 14:44:23 -0800

    Add react router dom and redux logger

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import ReduxPromise from 'redux-promise';
+import { createLogger } from 'redux-logger';
 import App from './app';
 import reducers from './reducers';
 
-const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
+const loggerMiddleware = createLogger();
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise, loggerMiddleware)(createStore);
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={createStoreWithMiddleware(reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )}>
     <App />
   </Provider>, document.getElementById('root'))
 
